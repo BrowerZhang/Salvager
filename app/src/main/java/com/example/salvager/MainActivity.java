eebdad726a1bacbd66b9a9f04925e3a591661624
14joragu <jorgeaguilar2974@gmail.com>
2022-04-05 21:36:15 -0400

    added navigation bar

--- app/src/main/java/com/example/salvager/MainActivity.java
+++ app/src/main/java/com/example/salvager/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.salvager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new FragmentHome()).commit();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId())
+                {
+                    case R.id.ic_home:
+                        fragment = new FragmentHome();
+                        break;
+                }
+                switch (item.getItemId())
+                {
+                    case R.id.ic_scanner:
+                        fragment = new FragmentScanner();
+                        break;
+                }
+                switch (item.getItemId())
+                {
+                    case R.id.ic_search:
+                        fragment = new FragmentSearch();
+                        break;
+                }
+                switch (item.getItemId())
+                {
+                    case R.id.ic_map:
+                        fragment = new FragmentMap();
+                        break;
+                }
+                switch (item.getItemId())
+                {
+                    case R.id.ic_faq:
+                        fragment = new FragmentFAQ();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+
+                return true;
+            }
+        });
     }
 }
