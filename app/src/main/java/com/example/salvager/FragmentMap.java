60cf25d1ff77f4f234b63243c0042cb34c1f064e
14joragu <jorgeaguilar2974@gmail.com>
2022-04-06 21:58:36 -0400

    added map activity

--- app/src/main/java/com/example/salvager/FragmentMap.java
+++ app/src/main/java/com/example/salvager/FragmentMap.java
@@ -9,12 +9,53 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class FragmentMap extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_map,container, attachToRoot false)
-        return
+        //initialize view
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragmentmap_layout,container,false);
+
+        //initialize map fragment
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.google_map);
+
+        //async map
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                //when map is loaded
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull LatLng latLng) {
+                        //when clicked on map
+                        //initialize marker options
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        //set postition of marker
+                        markerOptions.position(latLng);
+                        //set title of marker
+                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                        //remove all marker
+                        googleMap.clear();
+                        //animating to zoom the marker
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                        //add marker on map
+                        googleMap.addMarker(markerOptions);
+                    }
+                });
+            }
+        });
+
+
+        //return view
+        return view;
     }
 }
